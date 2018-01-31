82fa25d3b799e3df62eb3171d034d720118834ef
ayakatagiri <ayakata128@gmail.com>
2018-02-01 08:11:00 +0900

    引数と戻り値

---
+++ method/ArgumentReturn.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author aya
+ */
+public class ArgumentReturn extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    String[] str(String ID) {
+
+        String[] data1 = {"100", "200"};
+        String[] data2 = {"300", "400"};
+        String[] data3 = {"500", "600"};
+
+        String[] data4 = {""};
+        
+        if (ID == "100") {
+            data4 = data1;
+        } else if (ID == "300") {
+            data4 = data2;
+        } else if (ID == "500") {
+            data4 = data3;
+        }
+        return data4;
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+
+            String ID = "100";
+            
+            for (int i = 0; i < 3; i++) {
+                if (ID == "100") {
+                    str(ID);
+                    ID = "300";
+                } else if (ID == "300") {
+                    str(ID);
+                    ID = "500";
+                } else if (ID == "500") {
+                    str(ID);
+                }
+
+                String[] datadata = str(ID);
+                for (int j = 0; j < 2; j++) {
+                    out.print(datadata[j] + "<BR>");
+                }
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
